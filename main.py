2b28c11c08f92b1f9056c31a1d552cee44647021
Laura Porsch <laupor112@gmail.com>
2023-06-09 11:50:08 -0600

    added RESTful API, documentation and testing via Postman

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -34,10 +34,13 @@ class Cafes(db.Model):
     sockets = db.Column(db.Boolean)
     mountain_views = db.Column(db.Boolean)
 
-    # allows each Cafe Object to be identified by its name when printed
-    # otherwise it will only print out "<Cafes 1>"
     def __repr__(self):
-        return f"<Cafe> {self.cafe}"
+        """returns name of Cafe when printed instead of <Cafes ID>"""
+        return f"<Cafe> {self.name}"
+
+    def to_dict(self):
+        """converts DB-row Object to Dictionary"""
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
 # ---------------------------- SETUP FLASK FORM TO ADD CAFES ------------------------------- #
@@ -131,6 +134,97 @@ def contact():
         return render_template("contact.html", message_sent=True)
     else:
         return render_template("contact.html", message_sent=False)
+
+
+# ---------------------------- CREATE RESTful API ------------------------------- #
+# ---HTTP GET - Read Record(s)--- #
+@app.route("/get_all_cafes")
+def get_all_cafes():
+    """gets all Cafes Objects from DB and returns the Cafes Objects as JSON Objects"""
+    all_cafes = db.session.query(Cafes).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+@app.route("/search_cafe")
+def get_cafe_by_name():
+    """searches DB for a Cafes Object with a specific name and returns the Cafe Object as JSON Object, if no Cafes
+    Object is found, returns error 'Not found' message"""
+    query_name = request.args.get("name")
+    cafe = db.session.query(Cafes).filter_by(name=query_name).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not found": "Sorry, we don't have a cafe with that name."})
+
+
+# ---HTTP POST - Create Record(s)--- #
+@app.route("/add-api", methods=["POST"])
+def add_api_new_cafe():
+    """takes user JSON input and saves it as Cafes Object to DB, returns response 'success' message"""
+    new_cafe = Cafes(
+        name=request.form.get("name"),
+        location=request.form.get("location"),
+        maps_url=request.form.get("maps_url"),
+        image_url=request.form.get("image_url"),
+        open=request.form.get("open"),
+        close=request.form.get("close"),
+        wifi=bool(request.form.get("wifi")),
+        sockets=bool(request.form.get("sockets")),
+        mountain_views=bool(request.form.get("mountain_views"))
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added a new cafe"})
+
+
+# ---HTTP PATCH - Update Record--- #
+@app.route("/update-open/<cafe_id>", methods=["PATCH"])
+def update_open(cafe_id):
+    """gets data from requested Cafes Object and changes open attribute, returns 200 HTTP Code with 'success' message or
+    returns 404 HTTP Code with error message 'Not found' if requested Cafes Object does not exist"""
+    cafe_to_update = Cafes.query.get(cafe_id)
+    if cafe_to_update:
+        cafe_to_update.open = request.args.get("open")
+        db.session.commit()
+        return jsonify({"Success": "You successfully updated the opening time."}), 200
+    else:
+        return jsonify(error={"Not found": "The cafe you are looking for doesn't exist."}), 404
+
+
+@app.route("/update-close/<cafe_id>", methods=["PATCH"])
+def update_close(cafe_id):
+    """gets data from requested Cafes Object and changes close attribute, returns 200 HTTP Code with 'success' message or
+    returns 404 HTTP Code with error message 'Not found' if requested Cafes Object does not exist"""
+    cafe_to_update = Cafes.query.get(cafe_id)
+    if cafe_to_update:
+        cafe_to_update.close = request.args.get("close")
+        db.session.commit()
+        return jsonify({"Success": "You successfully updated the closing time."}), 200
+    else:
+        return jsonify(error={"Not found": "The cafe you are looking for doesn't exist."}), 404
+
+
+# ---HTTP DELETE - Delete Record--- #
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    """checks if user has the API key, returns HTTP Code 403 and error message 'Forbidden' if not, if API key is valid,
+     deletes requested Cafes Object from DB and returns 200 HTTP Code and success message, returns 404 HTTP Code and
+     error 'Not found' message if requested Cafes Object does not exist."""
+    if request.args.get("api_key") == "TopSecretAPIKey":
+        cafe_to_delete = Cafes.query.get(cafe_id)
+        if cafe_to_delete:
+            db.session.delete(cafe_to_delete)
+            db.session.commit()
+            return jsonify(success={"Success": "You successfully reported the cafe as closed."}), 200
+        else:
+            return jsonify(error={"Not found": "The cafe you are looking for doesn't exist."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed, make sure you have the right api-key."}), 403
+
+
+
+
+
 
 
 if __name__ == "__main__":
